7e2bc720c0afe88688bc145ef1ff87141c668b23
LiviaToric <vl2395@columbia.edu>
2018-10-15 21:44:31 -0400

    Update AOE_GAME_GAMMA__WORKING_TEST.ino

--- AOE_GAME_GAMMA__WORKING_TEST.ino
+++ AOE_GAME_GAMMA__WORKING_TEST.ino
@@ -1,4 +1,4 @@
-// IMPORTANT: ELEGOO_TFTLCD LIBRARY MUST BE SPECIFICALLY
+  // IMPORTANT: ELEGOO_TFTLCD LIBRARY MUST BE SPECIFICALLY
 // CONFIGURED FOR EITHER THE TFT SHIELD OR THE BREAKOUT BOARD.
 // SEE RELEVANT COMMENTS IN Elegoo_TFTLCD.h FOR SETUP.
 //Technical support:goodtft@163.com
@@ -116,24 +116,6 @@ const uint8_t student[] PROGMEM = {
   0x01, 0xe3, 0xbc, 0x00, 0x00, 0x7f, 0xf8, 0x00, 0x00, 0x1f, 0xe0, 0x00, 0x00, 0x00, 0x00, 0x00,
 };
 
-const int16_t TEST_WIDTH = 40;
-const int16_t TEST_HEIGHT = 40;
-const uint8_t test[] PROGMEM = {
-  0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xf8,
-  0x00, 0x00, 0x00, 0x1f, 0xf8, 0x00, 0x00, 0x00, 0x1f, 0xf8, 0x00, 0x00, 0x00, 0x1f, 0xf8, 0x00,
-  0x00, 0x00, 0x1f, 0xf8, 0x28, 0x00, 0x7c, 0x1f, 0xf8, 0x28, 0x00, 0x82, 0x1f, 0xf8, 0x6c, 0x01,
-  0x01, 0x1f, 0xf9, 0xc7, 0x02, 0x7c, 0x9f, 0xf8, 0x00, 0x02, 0x40, 0x9f, 0xf9, 0xc7, 0x02, 0x40,
-  0x9f, 0xf8, 0x6c, 0x02, 0x78, 0x9f, 0xf8, 0x28, 0x02, 0x40, 0x9f, 0xf8, 0x28, 0x01, 0x41, 0x1f,
-  0xf8, 0x00, 0x00, 0x82, 0x1f, 0xf8, 0x00, 0x00, 0x7c, 0x1f, 0xf8, 0x00, 0x00, 0x00, 0x1f, 0xf8,
-  0x00, 0x00, 0x00, 0x1f, 0xf8, 0x00, 0x00, 0x00, 0x1f, 0xf8, 0x1f, 0xc3, 0xf8, 0x1f, 0xf8, 0x05,
-  0x00, 0xa0, 0x1f, 0xf8, 0x05, 0x00, 0xa0, 0x1f, 0xf8, 0x05, 0x00, 0xa0, 0x1f, 0xf8, 0x05, 0x18,
-  0xa0, 0x1f, 0xf8, 0x05, 0x24, 0xa0, 0x1f, 0xf8, 0x05, 0x24, 0xa0, 0x1f, 0xf8, 0x05, 0x00, 0xa0,
-  0x1f, 0xf8, 0x05, 0x00, 0xa0, 0x1f, 0xf8, 0x00, 0x00, 0x00, 0x1f, 0xf8, 0x00, 0x00, 0x00, 0x1f,
-  0xf8, 0x00, 0x00, 0x00, 0x1f, 0xf8, 0x00, 0x00, 0x00, 0x1f, 0xf8, 0x00, 0x00, 0x00, 0x1f, 0xf8,
-  0x00, 0x00, 0x00, 0x1f, 0xf8, 0x00, 0x00, 0x00, 0x1f, 0xf8, 0x00, 0x00, 0x00, 0x1f, 0xff, 0xff,
-  0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
-};
-
 //SOUND VARIABLES
 const int16_t SOUND_SENSITIVITY = 40;
 const int16_t SAMPLE_WINDOW = 5; // Sample window width in mS (50 mS = 20Hz)
@@ -144,7 +126,7 @@ double soundValue;
 //STUDENT VARIABLES
 const int16_t STUDENT_COLOR = BLUE;
 double fallingRate = 0; //Student falling rate
-const int16_t SPEED = 3;
+const int16_t SPEED = 7;
 int16_t xStudent = 50; //Starting x position
 int16_t yStudent = TFT_HEIGHT - STUDENT_HEIGHT;
 const int16_t MAX_HEIGHT = 100;
@@ -156,84 +138,21 @@ int16_t yPillar = TFT_HEIGHT - random(50, 60);
 const int16_t PILLAR_WIDTH = 30;
 const int16_t PILLAR_COLOR = GREEN;
 
-//TEST VARIABLES
-//PILLARS VARIABLES
-int16_t movingRateTest = 5; //Obstacle moving rate
-int16_t xTest = TFT_WIDTH;
-int16_t yTest = TFT_HEIGHT - random(50, 60);
-const int16_t TEST_COLOR = CYAN;
+//PILLARS #2 VARIABLES
+int16_t xPillar2 = TFT_WIDTH;
+//int16_t yPillar2 = TFT_HEIGHT - random(50, 60);
 
 bool gameOver = false;
 int16_t tally = 0;
 
 //Array of Strings
-String words[7] = {"1007", "MULTI", "UWRITING", "AOE", "CHEM", "PHYSICS", "JAVA"};
-String className = "";
+char* words[7] = {"1007", "MULTI", "UW", "AOE", "CHEM", "JAVA"};
+int16_t randomNumber = random(6);
 
-void loop(void)
-{
-  tft.setTextColor(WHITE);
-  tft.setTextSize(2);
-  tft.setCursor(200, 0);
-  tft.println("Score: ");
-
-  tft.setTextColor(WHITE);
-  tft.setTextSize(1);
-
-  if (!gameOver)
-  {
-
-    if (yStudent <= TFT_HEIGHT - MAX_HEIGHT - STUDENT_HEIGHT)
-      fallingRate = SPEED;
-    else if (yStudent >= TFT_HEIGHT - STUDENT_HEIGHT)
-      fallingRate = 0;
-
-    if (fallingRate == 0)
-    {
-      soundValue = readSoundLevel();
-      if (soundValue > SOUND_SENSITIVITY)
-        fallingRate = -SPEED;
-    }
-
-    if (xPillar <= 50 + STUDENT_WIDTH && xPillar >= 50 - PILLAR_WIDTH)
-    {
-
-      tally += 1;
-      if (yStudent + STUDENT_HEIGHT > yPillar) {
-        gameOver = true;
-      }
-    }
-
-    scoreKeeper(tally);
-    drawStudent();
-
-    //drawPillar();
-    drawTest();//added
-
-    //Serial.println(soundValue);
-
-    xPillar -= movingRate;
-    //if the pillar is out of the screen,
-    if (xPillar < -PILLAR_WIDTH - 4)
-      xPillar = TFT_WIDTH;
-
-    xTest -= movingRateTest;
-    if (xTest < -TEST_WIDTH - 2 * SPEED)
-      xTest = TFT_WIDTH;//
-
-    yStudent += (int)fallingRate;
-    fallingRate += 0.4;
-    
-  }
-  else {
-    delay(1000);
-    gameEnd(tally);
-    tally = 0;
-    gameOver = false;
-    gameStart();
-  }
-
-}
+//next pillar
+int16_t next_xPillar = 100;
+bool next_Pillar = false;
+int16_t randomNumber2 = random(6);
 
 void drawStudent()
 {
@@ -248,18 +167,28 @@ void drawPillar()
 {
   tft.fillRect(xPillar, yPillar, PILLAR_WIDTH, TFT_HEIGHT - yPillar, PILLAR_COLOR);
   tft.setCursor(xPillar, yPillar + 30);
-  if (xPillar < TFT_WIDTH - 25) tft.println(1007);
+  if (xPillar < TFT_WIDTH - 25) {
+    tft.setTextColor(BLACK);
+    tft.println(words[randomNumber]);
+    tft.setTextColor(WHITE);
+  }
   tft.fillRect(xPillar + movingRate + PILLAR_WIDTH, yPillar, movingRate, TFT_HEIGHT - yPillar, BACKGROUND_COLOR);
 }
 
-void drawTest()
+void drawPillar2(bool state)
 {
-  tft.fillRect(xTest, yTest, TEST_WIDTH, TFT_HEIGHT - yTest, BACKGROUND_COLOR);
-  tft.Elegoo_GFX::drawBitmap(xTest, yTest, test, TEST_WIDTH, TEST_HEIGHT, TEST_COLOR);
-  tft.setCursor(xTest, yTest + 40);
-  //tft.fillRect(xTest, yTest, TEST_WIDTH, TFT_HEIGHT - yTest, TEST_COLOR);
-  tft.fillRect(xTest + movingRateTest + TEST_WIDTH, yTest, movingRateTest, TFT_HEIGHT - yTest, BACKGROUND_COLOR);
+  if(state){
+    tft.fillRect(xPillar2, yPillar, PILLAR_WIDTH, TFT_HEIGHT - yPillar, PILLAR_COLOR);
+    tft.setCursor(xPillar2, yPillar + 30);
+    if (xPillar2 < TFT_WIDTH - 25) {
+      tft.setTextColor(BLACK);
+      tft.println(words[randomNumber2]);
+      tft.setTextColor(WHITE);
+    }
+    tft.fillRect(xPillar2 + movingRate + PILLAR_WIDTH, yPillar, movingRate, TFT_HEIGHT - yPillar, BACKGROUND_COLOR);
+  }
 }
+
 double readSoundLevel()
 {
   ///MIC variables
@@ -293,11 +222,6 @@ double readSoundLevel()
 void gameFillScreen(int16_t color)
 {
   tft.fillScreen(color);
-}
-
-void gameDrawObsatcle(int x, int y)
-{
-  tft.fillRect(x, y, 40, 40, RED);
 }
 
 void scoreKeeper(int score) {
@@ -394,7 +318,90 @@ void gameEnd(int score) {
   displayGrade(score);
 
   xPillar = TFT_WIDTH;
+  xPillar2 = TFT_WIDTH;
   gameFillScreen(BACKGROUND_COLOR);
 }
 
+void loop(void)
+{
+  tft.setTextColor(WHITE);
+  tft.setTextSize(2);
+  tft.setCursor(200, 0);
+  tft.println("Score: ");
 
+  tft.setTextColor(WHITE);
+  tft.setTextSize(1);
+  
+
+  if (!gameOver)
+  {
+    
+    //if student is at max height
+    if (yStudent <= 124)
+      fallingRate = 1;
+    else if (yStudent >= TFT_HEIGHT - STUDENT_HEIGHT)
+    {
+      fallingRate = 0;
+      yStudent = TFT_HEIGHT - STUDENT_HEIGHT;
+    }
+    else
+      fallingRate += 0.2;
+
+    if (fallingRate == 0)
+    {
+      soundValue = readSoundLevel();
+      if (soundValue > SOUND_SENSITIVITY)
+        fallingRate = -SPEED;
+    }
+
+    if (xPillar <= 50 + STUDENT_WIDTH && xPillar >= 50 - PILLAR_WIDTH)
+    {
+
+      tally += 1;
+      if (yStudent + STUDENT_HEIGHT > yPillar) {
+        gameOver = true;
+      }
+    }
+    
+    if (next_Pillar && (xPillar2 <= 50 + STUDENT_WIDTH && xPillar2 >= 50 - PILLAR_WIDTH))
+    {
+      tally += 1;
+      if (yStudent + STUDENT_HEIGHT > yPillar) {
+        gameOver = true;
+      }
+    }
+
+    scoreKeeper(tally);
+    drawStudent();
+
+    drawPillar();
+    if(xPillar<TFT_WIDTH/2 || next_Pillar){next_Pillar = true;}
+    drawPillar2(next_Pillar); //summoning next pillar
+
+    xPillar -= movingRate;
+    //if the pillar is out of the screen,
+    if (xPillar < -PILLAR_WIDTH - 4) {
+      xPillar = TFT_WIDTH;
+      randomNumber = random(6);
+    }
+    
+    //Pillar#2
+    if(next_Pillar){
+      xPillar2 -= movingRate;
+    //if the pillar is out of the screen,
+      if (xPillar2 < -PILLAR_WIDTH - 4) {
+        xPillar2 = TFT_WIDTH;
+        randomNumber2 = random(6);
+      }
+      if (xPillar2==0){next_Pillar = false;}
+    }
+      yStudent += (int)fallingRate;
+  }
+  else {
+    delay(1000);
+    gameEnd(tally);
+    tally = 0;
+    gameOver = false;
+    gameStart();
+  }
+}
